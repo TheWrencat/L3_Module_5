fc090615e9b802c9150e16d34b8eff78d8a37b72
TheWrencat <league@WTS7.attlocal.net>
2018-04-12 19:40:21 -0700

    Did array overload and ducks, never run that program again

--- src/data_structures/ArrayOverload.java
+++ src/data_structures/ArrayOverload.java
@@ -7,7 +7,7 @@ public class ArrayOverload {
 	
 	public static void main(String[] args) {
 		ArrayList<String> over = new ArrayList<String>();
-		for(int i = 0; i<i-1; i++) {
+		for(int i = 0; i<i+1; i++) {
 			over.add("We're at " +i+ " and counting!");
 		}
 		
