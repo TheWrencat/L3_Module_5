35408485ca6447517597758f37b12bd549108edb
TheWrencat <league@WTS7.attlocal.net>
2018-04-12 19:01:19 -0700

    finished band names, started array overload

---
+++ src/data_structures/ArrayOverload.java
@@ -0,0 +1,16 @@
+package data_structures;
+
+import java.util.ArrayList;
+
+public class ArrayOverload {
+
+	
+	public static void main(String[] args) {
+		ArrayList<String> over = new ArrayList<String>();
+		for(int i = 0; i<i-1; i++) {
+			over.add("We're at " +i+ " and counting!");
+		}
+		
+	}
+	
+}
--- src/data_structures/BandNameGenerator.java
+++ src/data_structures/BandNameGenerator.java
@@ -1,5 +1,30 @@
 package data_structures;
 
-public class BandNameGenerator {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class BandNameGenerator {
+	ArrayList <String> adj;
+	ArrayList<String> noun;
+	private void setup() {
+		adj = new ArrayList<String>();
+		noun = new ArrayList<String>();
+		adj.add("Translucent");
+		adj.add("Stunning");
+		adj.add("Aquatic");
+		noun.add("Bees");
+		noun.add("Fruit");
+		noun.add("Burritos");
+		int r = new Random().nextInt(3);
+		String one = adj.get(r);
+		int s = new Random().nextInt(3);
+		String two = noun.get(s);
+		System.out.println("The "+one+" "+two+"!");
+	}
+	
+	public static void main(String[] args) {
+		BandNameGenerator band = new BandNameGenerator();
+		band.setup();
+	}
+	
 }
