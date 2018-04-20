7213ab4b90153240a2fed7d167ee4b6a23017645
williamyufan <league@WTS7.attlocal.net>
2018-04-19 19:56:38 -0700

    yeah

--- src/data_structures/iPodShuffle.java
+++ src/data_structures/iPodShuffle.java
@@ -6,14 +6,30 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.advanced.AdvancedPlayer;
 
 /* 1. Download the JavaZoom jar from here: http://bit.ly/javazoom
 * 2. Right click your project and add it as an External JAR (Under Java Build Path > Libraries).*/
 public class iPodShuffle {
-	
-	
+	JFrame frame;
+	JPanel panel;
+	JButton randomSong;
+	private void setup() {
+		frame = new JFrame();
+		panel = new JPanel();
+		randomSong = new JButton();
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(randomSong);
+		randomSong.addActionListener(this);
+	}
 	
 	public static void main(String[] args) throws IOException, JavaLayerException { // 3. Find an mp3 on your computer
 																					// or on the Internet.
@@ -23,12 +39,13 @@ public class iPodShuffle {
 		wii.setDuration(5);
 		// 5. Play the Song to test that it works. 
 		/**
-		 * 6. Congratulations on completing the sound check! * Now we want to make an
-		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
-		 * "Surprise Me!" button that will play a random song when it is clicked. * If
-		 * you're really cool, you can stop all the songs, before playing a new one on
-		 * subsequent button clicks.
+		 * 6. Congratulations on completing the sound check! 
+		 * * Now we want to make an
+		 * iPod Shuffle that plays random music. 
+		 * * Create an ArrayList of Songs and a "Surprise Me!" button that will play a random song when it is clicked. 
+		 * If you're really cool, you can stop all the songs, before playing a new one on subsequent button clicks.
 		 */
+		
 		
 	}
 	
