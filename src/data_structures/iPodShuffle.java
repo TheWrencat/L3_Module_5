02d7c7067443e62a150701676794d1f54041c365
williamyufan <league@WTS7.attlocal.net>
2018-04-19 19:56:11 -0700

    aggghhhh iPods

--- src/data_structures/iPodShuffle.java
+++ src/data_structures/iPodShuffle.java
@@ -18,6 +18,9 @@ public class iPodShuffle {
 	public static void main(String[] args) throws IOException, JavaLayerException { // 3. Find an mp3 on your computer
 																					// or on the Internet.
 		// 4. Use the Song class below to instantiate a Song.
+		Song wii = new Song("src/data_structures/Mii Channel Music.mp3");
+		wii.play();
+		wii.setDuration(5);
 		// 5. Play the Song to test that it works. 
 		/**
 		 * 6. Congratulations on completing the sound check! * Now we want to make an
@@ -26,8 +29,6 @@ public class iPodShuffle {
 		 * you're really cool, you can stop all the songs, before playing a new one on
 		 * subsequent button clicks.
 		 */
-		Song wii = new Song("src/data_structures/Mii Channel Music.mp3");
-		wii.play();
 		
 	}
 	
