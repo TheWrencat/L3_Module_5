fa9648b127b0c583dbf38d5fa72f680b1cc3900f
League <League@AdminisorsiMac2.attlocal.net>
2018-05-24 19:59:57 -0700

    snakes

--- src/SnakesOnAPlane.java
+++ src/SnakesOnAPlane.java
@@ -1,4 +1,11 @@
+import java.util.Random;
 
 public class SnakesOnAPlane {
 
+	class Randomness {
+		public boolean venom {
+			Boolean venomIs = new Random().nextBoolean();
+			return venomIs;
+		}
+	}
 }
