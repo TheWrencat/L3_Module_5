97b091ca6b3b23691ce93dad9954ff00f51168e8
League <League@AdminisorsiMac2.attlocal.net>
2018-06-14 20:00:26 -0700

    Started learning array lists review

---
+++ src/other/LearningArrayLists.java
@@ -0,0 +1,13 @@
+package other;
+
+import java.util.ArrayList;
+
+public class LearningArrayLists {
+
+	public static ArrayList createArrayList() {
+		ArrayList<String> testList = new ArrayList<String>();
+		return testList;
+	}
+
+	
+}
