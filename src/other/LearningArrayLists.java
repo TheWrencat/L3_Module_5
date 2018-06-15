a9e3bfd047e73f4632a4f57c54ab70d294060c97
League <League@AdminisorsiMac2.attlocal.net>
2018-06-14 20:00:40 -0700

    same thinggggg

--- src/other/LearningArrayLists.java
+++ src/other/LearningArrayLists.java
@@ -9,5 +9,37 @@ public class LearningArrayLists {
 		return testList;
 	}
 
+	public static void addToArrayList(ArrayList testList, String string) {
+		testList.add(string);
+	}
+
+	public static Object getNumberOfItems(ArrayList testList) {
+		return testList.size();
+	}
+
+	public static String getItem(ArrayList<String> testList, int i) {
+		return testList.get(i);
+	}
+
+	public static String iterateOver(ArrayList testList) {
+		
+		return null;
+	}
+
+	public static Object findItemOnList(ArrayList testList, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Object addAllInteger(ArrayList<Integer> testList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static String addAllString(ArrayList<String> testList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	
 }
